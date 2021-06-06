d887943927bbcf3f27bd08e5dbb0c198441b6756
graciebliss <gracieabliss@gmail.com>
2021-06-06 14:07:38 -0400

    final

--- src/main/java/apps.java
+++ src/main/java/apps.java
@@ -1,2 +1,18 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Gracie Bliss
+ */
+
+
+import java.util.*;
 public class apps {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is the quote? ");
+        String quote = in.nextLine();
+        System.out.print("Who said it? ");
+        String author = in.nextLine();
+        System.out.println(author+" says, \""+quote+"\"" );
+        in.close();
+    }
 }
